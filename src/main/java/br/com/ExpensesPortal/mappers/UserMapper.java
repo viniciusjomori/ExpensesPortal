cfb022341451c9a23afad91fcd885a5c987ebc59
Vinicius Jomori <yuitivinicius@gmail.com>
2024-03-19 16:19:55 -0300

    [change] uses attribute

--- src/main/java/br/com/ExpensesPortal/mappers/UserMapper.java
+++ src/main/java/br/com/ExpensesPortal/mappers/UserMapper.java
@@ -3,20 +3,17 @@ package br.com.ExpensesPortal.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-import br.com.ExpensesPortal.DTOs.infos.RoleInfo;
 import br.com.ExpensesPortal.DTOs.infos.UserInfo;
 import br.com.ExpensesPortal.DTOs.requests.UserRegisterDTO;
 import br.com.ExpensesPortal.DTOs.responses.UserResponseDTO;
-import br.com.ExpensesPortal.entities.RoleEntity;
 import br.com.ExpensesPortal.entities.UserEntity;
 
 import java.util.Collection;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = RoleMapper.class)
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
@@ -24,17 +21,10 @@ public interface UserMapper {
     @Mapping(target = "password", ignore = true)
     UserEntity toEntity(UserRegisterDTO dto);
 
-    @Mapping(source = "role", target = "role", qualifiedByName = "toRoleInfo")
     UserResponseDTO toResponseDTO(UserEntity entity);
-
     Collection<UserResponseDTO> toResponseDTO(Collection<UserEntity> entites);
 
     UserInfo toInfo(UserEntity entity);
     Collection<UserInfo> toInfo(Collection<UserEntity> entities);
-
-    @Named("toRoleInfo")
-    default RoleInfo toRoleInfo(RoleEntity roleEntity) {
-        return RoleMapper.INSTANCE.toInfo(roleEntity);
-    }
 
 }
